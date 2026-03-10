023928c93fc58651546b7c8f2e398e463c761eec
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-03-10 00:34:40 -0500

    refactoring class_selection node

--- src/util/utility.hpp
+++ src/util/utility.hpp
@@ -1,2 +1,16 @@
 #pragma once
 
+#include "godot_cpp/classes/node.hpp"
+
+#include <concepts>
+
+
+template <typename Parent, typename Child>
+requires std::derived_from<Parent, godot::Node> && std::derived_from<Child, godot::Node>
+Child* attach_owned_child_node(Parent* parent, Child* child, bool editable = true) {
+    if (!parent || !child)
+        return nullptr;
+
+
+
+}
