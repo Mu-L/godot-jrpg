18ab9ec18ec8953d7ab672e25fd5c9e9a05e5e7f
lilAbi <105619619+lilAbi@users.noreply.github.com>
2026-03-17 23:08:35 -0500

    creating battle system for game

--- src/entity/battle/tactics_tile_raycast.cpp
+++ src/entity/battle/tactics_tile_raycast.cpp
@@ -1,14 +1,23 @@
 #include "tactics_tile_raycast.hpp"
 
+#include "util/conversions.hpp"
+#include "godot_cpp/classes/ray_cast3d.hpp"
+
 godot::Array tog::TacticsTileRaycast::get_all_neighbors(float height) {
     godot::TypedArray<Node3D> neighbors;
 
     const Node3D* neighbors_node = this->get_node<godot::Node3D>(tog::node::name::RayCasting::Neighbors);
     assertion(neighbors_node, "MainMenu scene does not exist");
 
-    godot::Array children = neighbors_node->get_children();
+    auto children = neighbors_node->get_children();
 
     //godot::Array children = neighbors-
+
+    for (auto node : children) {
+
+        auto* child = rl::gdcast<godot::RayCast3D>(node.get_validated_object());
+
+    }
 
     return {};
 }
